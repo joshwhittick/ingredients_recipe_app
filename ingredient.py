b309039a326bbf069b5c50f026e59681f4fd478d
joshwhittickab <joshua.whittick@antibodies.com>
2023-11-24 14:21:00 +0000

    Update ingredient.py

--- ingredient.py
+++ ingredient.py
@@ -86,7 +86,7 @@ def Meal_Choser_Tab():
 
 def Recipe_Builder_Tab():
     def ingredients_page():
-        all_ingredients = get_availabe_ingredients()
+        all_ingredients = get_availabe_ingredients() + ['']
         st.title("Recipe Ingredients Input")
         recipe_name = st.text_input("Recipe Name:", key='recipe name')
         num_ingredients = st.number_input("Number of Ingredients:", min_value=1, max_value=50, step=1)
@@ -95,7 +95,7 @@ def Recipe_Builder_Tab():
 
         for i in range(num_ingredients):
             col1, col2, col3, col4 = st.columns(4)
-            ingredient_name = col1.selectbox(f"Name of Ingredient {i + 1}", [''] + all_ingredients, index=all_ingredients.index(''), key=f"ingredient_name_{i}")
+            ingredient_name = col1.selectbox(f"Name of Ingredient {i + 1}",  all_ingredients, index=all_ingredients.index(''), key=f"ingredient_name_{i}")
             if ingredient_name is '':
                 new_ingredient_name = col2.text_input(f"Name of Ingredient {i + 1} (New Ingredient)", key=f"ingredient_name_new_{i}")
 
